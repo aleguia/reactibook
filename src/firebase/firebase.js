473135b22afb72f6cfea6ac6503bb166a08d279d
Fernando Leguia <aleguia88@hotmail.com>
2018-04-16 19:15:58 -0500

    Inicializando proyecto

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBFlxJM-hTsyIksT1b9sdQVAYzenHoomeU",
+    authDomain: "react-firebase-authentic-269e4.firebaseapp.com",
+    databaseURL: "https://react-firebase-authentic-269e4.firebaseio.com",
+    projectId: "react-firebase-authentic-269e4",
+    storageBucket: "react-firebase-authentic-269e4.appspot.com",
+    messagingSenderId: "334559511061"
+};
+
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
